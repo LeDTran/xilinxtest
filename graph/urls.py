65df3f027a1fe3133f3c446d66d2d4d47f667a2f
le <ledtran99@gmail.com>
2017-10-16 00:17:46 -0700

    Almost finished. Just need some minor css

--- graph/urls.py
+++ graph/urls.py
@@ -2,9 +2,8 @@ from django.conf.urls import url
 from django.views.generic.base import TemplateView
 
 from . import views
-# from views import Graph
 
 urlpatterns = [
-    url(r'^$', views.Graph.as_view(), name='graph'),
-    # url(r'^$', views.Graph, name='graph'),
+    url(r'^$', views.Graph.as_view(), name='Graph'),
+    url(r'^export/csv/$', views.ExportGraphCSV, name='ExportGraphCSV'),
 ]
--- graph/views.py
+++ graph/views.py
@@ -1,118 +1,81 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView, View
+from django.db.models import Avg
+
 from graph.models import Student
+
+import csv
+import statistics
 
 import plotly.plotly as py
 import plotly.graph_objs as go
 
-import json
-
-
-# from rest_framework.views import APIView
-# from rest_framework.response import Response
+import simplejson as json
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
-
-# class Graph(TemplateView):
-
 class Graph(TemplateView):
     template_name = 'graph.html'
-
-    # def get_context_data(self, **kwargs):
-    #     context = super(Graph, self).get_context_data(**kwargs)
-
-    #     x = [-2,0,4,6,7]
-    #     y = [q**2-q+3 for q in x]
-    #     trace1 = go.Scatter(x=x, y=y, marker={'color': 'red', 'symbol': 104, 'size': "10"},
-    #                         mode="lines",  name='1st Trace')
-
-    #     data=go.Data([trace1])
-    #     layout=go.Layout(title="Meine Daten", xaxis={'title':'x1'}, yaxis={'title':'x2'})
-    #     figure=go.Figure(data=data,layout=layout)
-    #     div = py.plot(figure, auto_open=False, output_type='div')
-
-    #     context['graph'] = div
-
-    #     return context
 
     def get(self, request):
         students = Student.objects.all()
 
-        # test1_scores = Student.objects.all().values_list('test1')
-        test1 = Student.objects.values_list('test1', flat=True).order_by('test1')
-        # test1 = [2.37, 2.16, 4.82, 1.73, 1.04, 0.23, 1.32, 2.91, 0.11, 4.51, 0.51, 3.75, 1.35, 2.98, 4.50, 0.18, 4.66, 1.30, 2.06, 1.19]
+        test1 = Student.objects.values_list('test1', flat=True).order_by('test1')[::1]     
         test1_scores= json.dumps(test1)
 
-        test2_scores = Student.objects.values_list('test2', flat=True).order_by('test2')
-        test3_scores = Student.objects.values_list('test3', flat=True).order_by('test3')
+        test2 = Student.objects.values_list('test2', flat=True).order_by('test2')[::1] 
+        test2_scores= json.dumps(test2)
 
-        getSum(test1_scores)
+        test3 = Student.objects.values_list('test3', flat=True).order_by('test3')[::1] 
+        test3_scores= json.dumps(test3)
 
-        return render(request, "graph.html", {'students':students, 'test1_scores':test1_scores})
+        final = Student.objects.values_list('final', flat=True).order_by('final')[::1] 
+        final_scores= json.dumps(final)
 
-
-
-
-
-
-# def Graph(request):
-    # # template_name = 'graph.html'
-    # students = Student.objects.all()
-
-    # # test1_scores = Student.objects.all().values_list('test1')
-    # test1_scores = Student.objects.values_list('test1', flat=True).order_by('test1')
-    # test2_scores = Student.objects.values_list('test2', flat=True).order_by('test2')
-    # test3_scores = Student.objects.values_list('test3', flat=True).order_by('test3')
-
-    # getSum(test1_scores)
-
-    # return render(request, "graph.html", {'students':students, 'test1_scores':test1_scores})
-
-# _________________________
-# 
-#     trace0 = go.Box(
-#         y=[2.37, 2.16, 4.82, 1.73, 1.04, 0.23, 1.32, 2.91, 0.11, 4.51, 0.51, 3.75, 1.35, 2.98, 4.50, 0.18, 4.66, 1.30, 2.06, 1.19],
-#         name='Only Mean',
-#         marker=dict(
-#             color='rgb(8, 81, 156)',
-#         ),
-#         boxmean=True
-#     )
-#     trace1 = go.Box(
-#         y=[2.37, 2.16, 4.82, 1.73, 1.04, 0.23, 1.32, 2.91, 0.11, 4.51, 0.51, 3.75, 1.35, 2.98, 4.50, 0.18, 4.66, 1.30, 2.06, 1.19],
-#         name='Mean & SD',
-#         marker=dict(
-#             color='rgb(10, 140, 208)',
-#         ),
-#         boxmean='sd'
-#     )
-#     data = [trace0, trace1]
-#     py.iplot(data)
-#     return render(request, "graph.html", {'data':data})
-# 
-# ______________________________
-# 
-    # x = [-2,0,4,6,7]
-    # y = [q**2-q+3 for q in x]
-    # trace1 = go.Scatter(x=x, y=y, marker={'color': 'red', 'symbol': 104, 'size': "10"},
-    #                     mode="lines",  name='1st Trace')
-
-    # data=go.Data([trace1])
-    # layout=go.Layout(title="Meine Daten", xaxis={'title':'x1'}, yaxis={'title':'x2'})
-    # figure=go.Figure(data=data,layout=layout)
-    # data = py.plot(figure, auto_open=False, output_type='div')
-    # return render(request, "graph.html", {'data':data})
+        print(statistics.mean(test1))
 
 
+        return render(request, "graph.html", {'students':students, 'test1':test1_scores, 
+            'test2':test2_scores, 'test3':test3_scores, 'final':final_scores})
 
-def getSum(scores):
-    print(scores)
-    x=0
-    for i in scores:
-        # x = x + i
-        print(i)
-    print(x)
-    return 5
+        # Manual specifiction of boxplot points
+        # [min, q1, q1, median, q3, q3, max]
+
+def ExportGraphCSV(request):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="users.csv"'
+
+    writer = csv.writer(response)
+    writer.writerow(['Student', 'Test 1', 'Test 2', 'Test 3', 'Final'])
+
+    students = Student.objects.all().values_list('name', 'test1', 'test2', 'test3', 'final')
+    for student in students:
+        writer.writerow(student)
+
+
+    test1 = Student.objects.values_list('test1', flat=True).order_by('test1')[::1]     
+    test2 = Student.objects.values_list('test2', flat=True).order_by('test2')[::1] 
+    test3 = Student.objects.values_list('test3', flat=True).order_by('test3')[::1] 
+    final = Student.objects.values_list('final', flat=True).order_by('final')[::1] 
+ 
+    t1_avg = statistics.mean(test1)
+    t2_avg = statistics.mean(test2)
+    t3_avg = statistics.mean(test3)
+    final_avg = statistics.mean(final)
+
+    t1_sd = statistics.stdev(test1)
+    t2_sd = statistics.stdev(test2)
+    t3_sd = statistics.stdev(test3)
+    final_sd = statistics.stdev(final)
+
+    # t1_avg = students.aggregate(avg=Avg('test1'))['avg']
+
+    writer.writerow([])
+    writer.writerow(['', 'Test 1', 'Test 2', 'Test 3', 'Final'])
+    writer.writerow(['Mean', t1_avg, t2_avg, t3_avg, final_avg])
+    writer.writerow(['StDev', t1_sd, t2_sd, t3_sd, final_sd])
+
+    return response
+
